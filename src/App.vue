b50217a77ac87aa38af2d0e1ef6f99867018f19b
AlessioConsolo <alessioconsolo3@gmail.com>
2025-04-11 03:23:03 +0200

    [UPD] imported bootstrap on main.js, create component navbar.vue, changed title

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,18 @@
 <template>
   <div id="app">
-    <!-- Barra di navigazione (opzionale) -->
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/login">Login</router-link>
-    </nav>
-
-    <!-- Qui verranno renderizzate le views in base alla rotta -->
-    <router-view />
+    <div class=""><Navbar /> <router-view /></div>
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue"; // Importa il componente Navbar
+
 export default {
   name: "App",
+  components: {
+    Navbar, // Registra il componente Navbar
+  },
 };
 </script>
 
-<style>
-/* Stili di base, aggiungi altri stili se necessario */
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-nav {
-  display: flex;
-  justify-content: center;
-  gap: 1rem;
-  margin-bottom: 20px;
-}
-
-router-link {
-  text-decoration: none;
-  color: #42b983;
-}
-</style>
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,71 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">🎵 Clonify</RouterLink>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/">Home</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/song">Canzoni</RouterLink>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav ms-auto">
+          <template v-if="isLoggedIn">
+            <li class="nav-item">
+              <button class="btn btn-outline-light" @click="logout">
+                Logout
+              </button>
+            </li>
+          </template>
+          <template v-else>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/login">Login</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/register"
+                >Registrati</RouterLink
+              >
+            </li>
+          </template>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  computed: {
+    isLoggedIn() {
+      return !!localStorage.getItem("token");
+    },
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem("token");
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.navbar-brand {
+  font-weight: bold;
+  font-size: 1.3rem;
+}
+</style>
